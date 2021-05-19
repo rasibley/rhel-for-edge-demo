87c448791b38a7bc7ec6bb520c1fc5d213987033
Rachel Sibley <rasibley@redhat.com>
2021-05-19 15:37:12 -0400

    Run ostree.sh on vagrant setup

--- Vagrantfile
+++ Vagrantfile
@@ -5,5 +5,10 @@ Vagrant.configure("2") do |config|
     config.vm.provision :shell, path: "bootstrap.sh", 
          args: "#{ENV['USER']} #{ENV['PASS']}"
     config.vm.network "forwarded_port", guest: 9090, host: 9091
-    config.vm.synced_folder ".", "/vagrant"
+    # enable nested virtualization
+    config.vm.provider :libvirt do |vm|
+         vm.memory = 8192
+         vm.cpus = 4
+         vm.nested = true
+    end
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -8,18 +8,11 @@ subscription-manager role --set="Red Hat Enterprise Linux Server"
 subscription-manager service-level --set="Self-Support"
 subscription-manager usage --set="Development/Test"
 subscription-manager attach
-yum -y install osbuild-composer composer-cli cockpit-composer bash-completion
-systemctl enable --now osbuild-composer.socket
-systemctl enable --now cockpit.socket
 
-cd /vagrant
-composer-cli blueprints push blueprint.toml
-echo "Starting compose build"
-uuid=$(composer-cli compose start-ostree Edge rhel-edge-commit | awk '{print $2}')
-
-until [ "$(composer-cli compose status | awk '{print $2}')" = "FINISHED" ]; do
-   echo -n .
-   sleep 60
-done
+sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm; sudo dnf config-manager --set-enabled epel
+sudo dnf install -y ansible jq qemu-img qemu-kvm libvirt-client libvirt-daemon-kvm virt-install git
+git clone https://github.com/virt-s1/rhel-edge.git; cd rhel-edge
+chmod 600 key/ostree_key
+./ostree.sh
 
 echo "Done"
